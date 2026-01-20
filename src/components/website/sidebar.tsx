41382822987bac624b254fa8da095711c9378105
k4rtikay <katt1bh4i@gmail.com>
2026-01-20 16:48:49 +0000

    style: adjust padding

--- src/components/website/sidebar.tsx
+++ src/components/website/sidebar.tsx
@@ -60,7 +60,7 @@ export function Sidebar({...props}){
     ]
 
     return(
-        <aside className="w-64 px-8 py-8 h-full text-sm overflow-y-auto">   
+        <aside className="w-64 pt-8 pb-16 px-8 lg:pt-12 h-full text-sm overflow-y-auto">   
                 <ul className="flex flex-col gap-8">
                     {
                         comps.map((comp)=>{
