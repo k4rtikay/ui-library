fbfd62984514310ddb166d9df172f84776a8add3
k4rtikay <katt1bh4i@gmail.com>
2026-01-20 16:48:31 +0000

    feat: add tailwind/typography styles and introduction docs

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -7,8 +7,10 @@ export default function DocsLayout({
 }>) {
     return (
         <div className="flex">
-            <Sidebar/>
-            <div className="flex-1">{children}</div>
+            <Sidebar />
+            <main className='prose prose-zinc dark:prose-invert prose-h1:scroll-m-20 prose-h1:text-2xl prose-h1:font-semibold prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-xl prose-h2:font-medium prose-h3:scroll-m-20 prose-h3:text-base prose-h3:font-medium prose-h4:scroll-m-20 prose-h5:scroll-m-20 prose-h6:scroll-m-20 prose-strong:font-medium prose-table:block prose-table:overflow-y-auto mr-0 max-w-full min-w-0 flex-1 pt-8 pb-16 lg:pt-12 xl:mr-12 xl:max-w-2xl'>
+                {children}
+            </main>
         </div>
     );
 }
