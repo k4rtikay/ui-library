3e9d631b379f84824930e08d57eb00a5425768b1
k4rtikay <katt1bh4i@gmail.com>
2026-01-22 16:54:58 +0000

    feat: add dark mode

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/website/mode-toggle";
 import { Sidebar } from "@/components/website/sidebar";
 
 export default function DocsLayout({
@@ -7,6 +8,7 @@ export default function DocsLayout({
 }>) {
     return (
         <div className="flex">
+            <ModeToggle/>
             <Sidebar />
             <main className='prose prose-zinc dark:prose-invert prose-h1:scroll-m-20 prose-h1:text-2xl prose-h1:font-semibold prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-xl prose-h2:font-medium prose-h3:scroll-m-20 prose-h3:text-base prose-h3:font-medium prose-h4:scroll-m-20 prose-h5:scroll-m-20 prose-h6:scroll-m-20 prose-strong:font-medium prose-table:block prose-table:overflow-y-auto mr-0 max-w-full min-w-0 flex-1 pt-8 pb-16 lg:pt-12 xl:mr-12 xl:max-w-2xl'>
                 {children}
