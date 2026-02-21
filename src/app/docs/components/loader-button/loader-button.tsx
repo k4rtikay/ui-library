e8ca604729b7dca4fc3511601edc9d06d68cd9cb
k4rtikay <katt1bh4i@gmail.com>
2026-02-21 09:39:18 +0000

    feat: create loading spinner

--- src/app/docs/components/loader-button/loader-button.tsx
+++ src/app/docs/components/loader-button/loader-button.tsx
@@ -37,18 +37,18 @@ export default function LoaderButton({ state = 'idle', onClick }: LoaderButtonPr
 
     return (
         <motion.button
-            onClick={() => simulateHandleClick(2000, false)}
+            onClick={() => simulateHandleClick(3000, false)}
             disabled={buttonState !== 'idle'}
             layout
             variants={buttonVariants}
             animate={buttonState}
-            className="w-64 text-background px-4 py-2 rounded-full"
+            className="w-48 h-12 rounded-full font-semibold"
         >
             <AnimatePresence
             mode="wait"
             >
                 {buttonState === 'idle' &&
-                    <motion.span
+                    <motion.div
                     key={'idle'}
                     variants={contentVariants}
                     initial="initial"
@@ -56,7 +56,7 @@ export default function LoaderButton({ state = 'idle', onClick }: LoaderButtonPr
                     exit="exit"
                     >
                         Loader Button
-                    </motion.span>
+                    </motion.div>
                 }
                 {buttonState === 'loading' &&
                     <motion.div
@@ -65,8 +65,9 @@ export default function LoaderButton({ state = 'idle', onClick }: LoaderButtonPr
                     initial="initial"
                     animate="animate"
                     exit="exit"
+                    className="w-full flex items-center justify-center gap-2"
                     >
-                        Loading...
+                        <LoadingSpinner /> Submitting...
                     </motion.div>
                 }
                 {buttonState === 'success' &&
@@ -99,26 +100,29 @@ export default function LoaderButton({ state = 'idle', onClick }: LoaderButtonPr
 const buttonVariants = {
     idle: {
         scale: 1,
-        backgroundColor: 'var(--foreground)',
+        backgroundColor: 'var(--muted)',
+        color: 'var(--foreground)',
     },
     loading: {
-        scale: [1,1.05,1],
+        scale: [1,1.02,1],
+        color: 'var(--foreground)',
         transition: {
             scale: {
                 repeat: Infinity,
-                duration: 1,
+                duration: 1.25,
                 ease: "easeInOut",
             }
         } as const,
-        backgroundColor: 'var(--foreground)',
+        backgroundColor: 'var(--muted)',
     },
     success: {
         scale: [1,1.15,1],
-        backgroundColor: '#98D8AA',
+        backgroundColor: '#a0e2b3ff',
+        color: '#1f3024ff',
         transition: {
             scale: {
                 duration: 0.25,
-                ease: "easeOut",
+                ease: "easeInOut",
             }
         } as const,
     },
@@ -127,24 +131,73 @@ const buttonVariants = {
         x: [0, -4, 4, -4, 4, 0],
         transition: {
             x: {
-                duration: 0.4,
+                duration: 0.25,
             }
         } as const,
-        backgroundColor: '#F07167',
+        backgroundColor: '#efb2adff',
+        color: '#4a1b1bff',
     },
 };
 
 const contentVariants = {
     initial: {
         opacity: 0,
-        y: 10,
     },
     animate: {
         opacity: 1,
-        y: 0,
+        transition: {
+            duration: 0.1,
+            ease: "easeOut",
+        } as const,
     },
     exit: {
         opacity: 0,
-        y: -10,
+        transition: {
+            duration: 0.1,
+            ease: "easeOut",
+        } as const,
+    },
+}
+
+export function LoadingSpinner(){
+    return (
+        <div className="flex w-fit p-2 rounded-full h-full items-center justify-center bg-foreground">
+            {[...Array(4)].map((_,i) => {
+                return (
+                    <motion.div
+                    key={i}
+                    variants={loaderVariants}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    custom={i}
+                    className="w-1 h-4 rounded-full mx-0.5 bg-background"
+                    />
+                );
+            })}
+        </div>
+    );
+}
+
+const loaderVariants = {
+    initial: {
+        opacity: 0,
+        x: -10,
+    },
+    animate: (i:number) => ({
+        opacity: 1,
+        x: 0,
+        scaleY: [0.5, 1, 0.5],
+        transition: {
+            scaleY: {
+                repeat: Infinity,
+                duration: 1,
+                ease: "easeInOut",
+                delay: i * 0.1,
+            }
+        } as const,
+    }),
+    exit: {
+        opacity: 0,
     },
 }
