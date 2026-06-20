2dc63b4bb2e8a6cd93a8503eeee753248b1a8b80
k4rtikay <katt1bh4i@gmail.com>
2026-06-20 06:36:36 +0000

    feat: change cta with a star reveal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Link from "next/link";
 import Image from "next/image";
 import BgImage from "../../public/waves-green.png";
-import { ChevronRight, Github } from "lucide-react";
 import { Instrument_Serif } from "next/font/google";
+import { GithubStarButton } from "@/components/github-star-button";
 
 const instrumentSerif = Instrument_Serif({
     subsets: ["latin"],
@@ -22,19 +21,9 @@ export default function Home() {
                 priority
                 className="animate-hero-zoom object-cover -z-10"
             />
-            <header className="bg-none w-full max-w-7xl flex flex-row-reverse justify-between items-center pt-8 z-50">
-                <a
-                    href="https://github.com/k4rtikay/ui-library"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="p-2 bg-secondary/50 hover:bg-secondary/80 scale-100 shadow-[inset_0px_2px_8px_0px_rgba(255,255,255,0.95),0px_2px_16px_0px_rgba(0,0,0,0.1)] ring-1 ring-border rounded-xl transition-all duration-240 hover:scale-105 ease-out"
-                >
-                    <Github className="h-6 w-6" />
-                </a>
-            </header>
+            <header className="bg-none w-full max-w-7xl flex flex-row-reverse justify-between items-center pt-8 z-50" />
 
             <main className="relative w-full flex-1 flex flex-col items-center justify-start overflow-hidden">
-
                 <div className="text-black relative z-10 flex flex-col items-center justify-center md:justify-start text-center px-6 mt-24">
                     <h1
                         className={`text-6xl md:text-8xl mb-4 tracking-tight leading-none`}
@@ -63,13 +52,7 @@ export default function Home() {
                         Motion.
                     </p>
 
-                    <Link
-                        href="/docs"
-                        className="group text-sm font-medium tracking-wide leading-0.5 ring-2 ring-white/10 bg-primary text-primary-foreground px-4 py-3 rounded-xl flex gap-0.5 items-center justify-center hover:opacity-90 transition-all duration-280 ease-out shadow-[inset_0px_2px_8px_0px_rgba(255,255,255,0.55),0px_2px_16px_0px_rgba(0,0,0,0.1)]"
-                    >
-                        Get Started
-                        <ChevronRight className="h-5 w-5 transition-transform group-hover:translate-x-1" />
-                    </Link>
+                    <GithubStarButton />
                 </div>
             </main>
         </div>
