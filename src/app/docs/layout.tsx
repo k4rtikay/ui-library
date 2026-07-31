07c9288c693b2c9e2af66d0df63ce35dc0eac0a3
k4rtikay <katt1bh4i@gmail.com>
2026-07-31 15:10:46 +0000

    chore: hide experimental components

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -10,26 +10,26 @@ const Routes: RouteItem[] = [
         href: "/docs/components/loader-button",
         category: "Buttons",
     },
-    {
-        title: "Hello Button",
-        href: "/docs/components/hello-button",
-        category: "Buttons",
-    },
+    // {
+    //     title: "Hello Button",
+    //     href: "/docs/components/hello-button",
+    //     category: "Buttons",
+    // },
     {
         title: "Card Stack",
         href: "/docs/components/card-stack",
         category: "Cards",
     },
-    {
-        title: "Shared Menu",
-        href: "/docs/components/shared-menu",
-        category: "Menus",
-    },
-    {
-        title: "Stagger Reveal",
-        href: "/docs/components/stagger-reveal",
-        category: "Animations",
-    },
+    // {
+    //     title: "Shared Menu",
+    //     href: "/docs/components/shared-menu",
+    //     category: "Menus",
+    // },
+    // {
+    //     title: "Stagger Reveal",
+    //     href: "/docs/components/stagger-reveal",
+    //     category: "Animations",
+    // },
     {
         title: "Pinned List",
         href: "/docs/components/pinned-list",
