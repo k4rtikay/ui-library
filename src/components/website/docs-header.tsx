a73e80c84edf7894fded696db0b241b3e0448ebf
k4rtikay <katt1bh4i@gmail.com>
2026-08-13 08:18:54 +0000

    style: docs header

--- src/components/website/docs-header.tsx
+++ src/components/website/docs-header.tsx
@@ -1,39 +1,50 @@
 import Link from "next/link";
 import Image from "next/image";
 import { ModeToggle } from "@/components/website/mode-toggle";
-// import MobileNav from "@/components/website/mobile-nav";
+import MobileNav from "@/components/website/mobile-nav";
 import { SpotlightTrigger } from "@/components/website/spotlight-trigger";
-import { Github } from "lucide-react";
+import { Github, Sidebar } from "lucide-react";
+import { Button } from "../ui/button";
 
 export default function DocsHeader() {
     return (
-        <header className="sticky backdrop-blur-md z-50 top-4 w-full bg-card/70 border-border border-px rounded-xl shadow-sm max-w-1/2 mx-auto my-8">
-            <div className="flex items-center justify-between px-4 py-1 text-sm">
-                <div className="flex items-center gap-6">
-                    {/*<MobileNav />*/}
-                    <Link
-                        href="/"
-                        className="flex items-center gap-2"
-                    >
-                        <Image
-                            src="/flow-kit-icon.png"
-                            alt="Flow Kit Logo"
-                            width={24}
-                            height={24}
-                        />
-                    </Link>
-                </div>
-                <div className="flex items-center gap-4">
-                    <SpotlightTrigger />
-                    <ModeToggle />
+        <header className="sticky w-full z-50 top-8 p-2 my-4 overflow-hidden flex justify-between items-center">
+            <div className="flex items-center gap-4">
+                <MobileNav />
+                <Button
+                    variant="outline"
+                    size="icon"
+                    className="rounded-full bg-secondary text-secondary-foreground dark:bg-secondary dark:text-secondary-foreground shadow-md dark:shadow-(--elevation-md)"
+                >
+                    <Sidebar />
+                </Button>
+                <Link href="/" className="flex items-center rounded-full">
+                    <Image
+                        src="/waves-blue.png"
+                        alt="Flow Kit Logo"
+                        width={32}
+                        height={32}
+                        className="size-8 shrink-0 rounded-full object-cover h-9 w-9"
+                    />
+                </Link> 
+            </div>
+            <div className="flex items-center gap-4">
+                <SpotlightTrigger />
+                <ModeToggle />
+                <Button
+                    variant="outline"
+                    size="icon"
+                    className="rounded-full bg-secondary text-secondary-foreground dark:bg-secondary dark:text-secondary-foreground shadow-md dark:shadow-(--elevation-md)"
+                    asChild
+                >
                     <Link
                         href="https://github.com/k4rtikay/ui-library"
-                        className="flex items-center gap-2 hover:opacity-80 transition-opacity"
+                        className="flex items-center"
                     >
                         <Github size={16} />
                     </Link>
-                </div>
-            </div>  
+                </Button>
+            </div>
         </header>
     );
 }
